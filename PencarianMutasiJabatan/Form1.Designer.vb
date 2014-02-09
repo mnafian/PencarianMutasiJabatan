dbf1e627fcfc3963b4e08ae2afe4585061068681
M. Nafian Wildana <semutliar2406@gmail.com>
2014-02-10 01:33:18 +0700

    First commit

---
+++ PencarianMutasiJabatan/Form1.Designer.vb
@@ -0,0 +1,321 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.TabControl1 = New System.Windows.Forms.TabControl()
+        Me.TabPage1 = New System.Windows.Forms.TabPage()
+        Me.TabPage2 = New System.Windows.Forms.TabPage()
+        Me.DataGridView2 = New System.Windows.Forms.DataGridView()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.TabPage3 = New System.Windows.Forms.TabPage()
+        Me.DataGridView3 = New System.Windows.Forms.DataGridView()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.TabPage4 = New System.Windows.Forms.TabPage()
+        Me.DataGridView4 = New System.Windows.Forms.DataGridView()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button4 = New System.Windows.Forms.Button()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        Me.TabControl1.SuspendLayout()
+        Me.TabPage1.SuspendLayout()
+        Me.TabPage2.SuspendLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage3.SuspendLayout()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.TabPage4.SuspendLayout()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(3, 34)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(1083, 289)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(131, 8)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(358, 6)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Cetak"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(6, 11)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(119, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Cari Berdasar Nama"
+        '
+        'Panel1
+        '
+        Me.Panel1.Controls.Add(Me.TabControl1)
+        Me.Panel1.Location = New System.Drawing.Point(18, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1103, 355)
+        Me.Panel1.TabIndex = 4
+        '
+        'TabControl1
+        '
+        Me.TabControl1.Controls.Add(Me.TabPage1)
+        Me.TabControl1.Controls.Add(Me.TabPage2)
+        Me.TabControl1.Controls.Add(Me.TabPage3)
+        Me.TabControl1.Controls.Add(Me.TabPage4)
+        Me.TabControl1.Location = New System.Drawing.Point(3, 3)
+        Me.TabControl1.Name = "TabControl1"
+        Me.TabControl1.SelectedIndex = 0
+        Me.TabControl1.Size = New System.Drawing.Size(1097, 349)
+        Me.TabControl1.TabIndex = 4
+        '
+        'TabPage1
+        '
+        Me.TabPage1.Controls.Add(Me.DataGridView1)
+        Me.TabPage1.Controls.Add(Me.TextBox1)
+        Me.TabPage1.Controls.Add(Me.Label1)
+        Me.TabPage1.Controls.Add(Me.Button1)
+        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage1.Name = "TabPage1"
+        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage1.Size = New System.Drawing.Size(1089, 323)
+        Me.TabPage1.TabIndex = 0
+        Me.TabPage1.Text = "Mutasi Jabatan"
+        Me.TabPage1.UseVisualStyleBackColor = True
+        '
+        'TabPage2
+        '
+        Me.TabPage2.Controls.Add(Me.DataGridView2)
+        Me.TabPage2.Controls.Add(Me.TextBox2)
+        Me.TabPage2.Controls.Add(Me.Label2)
+        Me.TabPage2.Controls.Add(Me.Button2)
+        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage2.Name = "TabPage2"
+        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPage2.Size = New System.Drawing.Size(1089, 323)
+        Me.TabPage2.TabIndex = 1
+        Me.TabPage2.Text = "Proses Mutasi Jabatan"
+        Me.TabPage2.UseVisualStyleBackColor = True
+        '
+        'DataGridView2
+        '
+        Me.DataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView2.Location = New System.Drawing.Point(3, 36)
+        Me.DataGridView2.Name = "DataGridView2"
+        Me.DataGridView2.Size = New System.Drawing.Size(1083, 284)
+        Me.DataGridView2.TabIndex = 7
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(131, 9)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(6, 12)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(119, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Cari Berdasar Nama"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(358, 7)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Cetak"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'TabPage3
+        '
+        Me.TabPage3.Controls.Add(Me.DataGridView3)
+        Me.TabPage3.Controls.Add(Me.TextBox3)
+        Me.TabPage3.Controls.Add(Me.Label3)
+        Me.TabPage3.Controls.Add(Me.Button3)
+        Me.TabPage3.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage3.Name = "TabPage3"
+        Me.TabPage3.Size = New System.Drawing.Size(1089, 323)
+        Me.TabPage3.TabIndex = 2
+        Me.TabPage3.Text = "Pengajuan Kecelakaan Kerja"
+        Me.TabPage3.UseVisualStyleBackColor = True
+        '
+        'DataGridView3
+        '
+        Me.DataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView3.Location = New System.Drawing.Point(3, 34)
+        Me.DataGridView3.Name = "DataGridView3"
+        Me.DataGridView3.Size = New System.Drawing.Size(1083, 284)
+        Me.DataGridView3.TabIndex = 11
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(131, 7)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox3.TabIndex = 8
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(6, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(119, 13)
+        Me.Label3.TabIndex = 10
+        Me.Label3.Text = "Cari Berdasar Nama"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(358, 5)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(75, 23)
+        Me.Button3.TabIndex = 9
+        Me.Button3.Text = "Cetak"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'TabPage4
+        '
+        Me.TabPage4.Controls.Add(Me.DataGridView4)
+        Me.TabPage4.Controls.Add(Me.TextBox4)
+        Me.TabPage4.Controls.Add(Me.Label4)
+        Me.TabPage4.Controls.Add(Me.Button4)
+        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
+        Me.TabPage4.Name = "TabPage4"
+        Me.TabPage4.Size = New System.Drawing.Size(1089, 323)
+        Me.TabPage4.TabIndex = 3
+        Me.TabPage4.Text = "Proses Kecelakaan Kerja"
+        Me.TabPage4.UseVisualStyleBackColor = True
+        '
+        'DataGridView4
+        '
+        Me.DataGridView4.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.DataGridView4.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView4.Location = New System.Drawing.Point(3, 34)
+        Me.DataGridView4.Name = "DataGridView4"
+        Me.DataGridView4.Size = New System.Drawing.Size(1083, 284)
+        Me.DataGridView4.TabIndex = 11
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(131, 7)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(221, 20)
+        Me.TextBox4.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(6, 10)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(119, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Cari Berdasar Nama"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(358, 5)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "Cetak"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1133, 381)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.Text = "Pencarian "
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.TabControl1.ResumeLayout(False)
+        Me.TabPage1.ResumeLayout(False)
+        Me.TabPage1.PerformLayout()
+        Me.TabPage2.ResumeLayout(False)
+        Me.TabPage2.PerformLayout()
+        CType(Me.DataGridView2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage3.ResumeLayout(False)
+        Me.TabPage3.PerformLayout()
+        CType(Me.DataGridView3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.TabPage4.ResumeLayout(False)
+        Me.TabPage4.PerformLayout()
+        CType(Me.DataGridView4, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents DataGridView1 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents TabControl1 As System.Windows.Forms.TabControl
+    Friend WithEvents TabPage1 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage2 As System.Windows.Forms.TabPage
+    Friend WithEvents DataGridView2 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents TabPage3 As System.Windows.Forms.TabPage
+    Friend WithEvents TabPage4 As System.Windows.Forms.TabPage
+    Friend WithEvents DataGridView3 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox3 As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents DataGridView4 As System.Windows.Forms.DataGridView
+    Friend WithEvents TextBox4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+
+End Class
---
+++ PencarianMutasiJabatan/Form1.vb
@@ -0,0 +1,219 @@
+﻿Imports Oracle.DataAccess.Client
+Imports Oracle.DataAccess.Types
+
+'Form2 untuk crystalreport cetak mutasi jabatan
+'Form3 untuk crystalreport cetak proses mutasi jabatan
+'Form4 untuk crystalreport cetak kecelakaan kerja
+'Form5 untuk crystalreport cetak proses kecelakaan
+
+Public Class Form1
+    Dim kueri As String
+    Dim sql As String
+
+    'Method untuk menampilkan hasil kueri pencarian data mutasi ke dbgridview saja
+    Sub CariMutasi()
+        kueri = TextBox1.Text.ToString.ToUpper
+        sql = "select a.id_tran,a.status,to_char(a.tgl_cr, 'fmdd MON yyyy')as tgl_cr,a.npk_1,b.nama, d.nama_departemen," &
+                "a.npk_2, e.nama as nama_proses,f.n_jabatan as jabatan_lama, g.n_jabatan as jabatan_baru, a.ket " &
+                "from t_mutasi_jab a join karyawan b on a.npk_1=b.npk " &
+                "join jab c on b.jab=c.id_jabatan join dept d on b.dept=d.id_departemen " &
+                "join karyawan e on e.npk=a.npk_2 join jab f on a.n_jab=f.id_jabatan " &
+                "join jab g on a.o_jab=g.id_jabatan where b.nama like '%" + kueri + "%'"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_mutasi_jab")
+        DataGridView1.DataSource = ds.Tables("t_mutasi_jab")
+        DataGridView1.ReadOnly = True
+        DataGridView1.Columns(0).HeaderText = "KODE TRANSAKSI" '
+        DataGridView1.Columns(1).HeaderText = "STATUS"
+        DataGridView1.Columns(2).HeaderText = "TANGGAL"
+        DataGridView1.Columns(3).HeaderText = "NPK PEMINTA"
+        DataGridView1.Columns(4).HeaderText = "NAMA"
+        DataGridView1.Columns(5).HeaderText = "DEPARTEMEN"
+        DataGridView1.Columns(6).HeaderText = "NPK PROSES"
+        DataGridView1.Columns(7).HeaderText = "NAMA"
+        DataGridView1.Columns(8).HeaderText = "JABATAN LAMA"
+        DataGridView1.Columns(9).HeaderText = "JABATAN BARU"
+        DataGridView1.Columns(10).HeaderText = "KETERANGAN"
+    End Sub
+
+    'Method untuk menampikan hasil pencarian proses mutasi ke dbgridview saja
+    Sub CariProsesMutasi()
+        kueri = TextBox2.Text.ToString.ToUpper
+        sql = "select b.id_tran, b.status,to_char(b.tgl_cr, 'fmdd MON yyyy')as tgl_cr, a.id_tran as kode_permintaan,k.npk, k.nama," &
+            " b.no_tap, b.tgl_tap, b.ket from t_mutasi_jab a left join t_mutasi_jab_pro b on b.id_minta_mutasi=a.id_tran" &
+            " left join karyawan k on k.npk=a.npk_2 where b.status='OPEN' and k.nama like '%" + kueri + "%'"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_mutasi_jab_pro")
+        DataGridView2.DataSource = ds.Tables("t_mutasi_jab_pro")
+        DataGridView2.ReadOnly = True
+        DataGridView2.Columns(0).HeaderText = "KODE TRANSAKSI" '
+        DataGridView2.Columns(1).HeaderText = "STATUS"
+        DataGridView2.Columns(2).HeaderText = "TANGGAL"
+        DataGridView2.Columns(3).HeaderText = "KODE PERMINTAAN" '
+        DataGridView2.Columns(4).HeaderText = "NPK"
+        DataGridView2.Columns(5).HeaderText = "NAMA"
+        DataGridView2.Columns(6).HeaderText = "NO TAP"
+        DataGridView2.Columns(7).HeaderText = "TANGGAL TAP"
+        DataGridView2.Columns(8).HeaderText = "KETERANGAN"
+    End Sub
+
+    'Method untuk menampikan hasil kueri pencarian kecelakaan kerja ke Datagridview
+    Sub CariKecelakaanKerja()
+        kueri = TextBox3.Text.ToString.ToUpper
+        sql = "select a.id_tran,a.status,a.tgl_cr,a.no_kpa,a.npk,b.nama,a.tkp,a.t_rawat,a.tgl_tkp,a.ket" &
+            " from t_celaka a left join karyawan b on b.npk=a.npk where b.nama like '%" + kueri + "%' order by a.id_tran"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_celaka")
+        DataGridView3.DataSource = ds.Tables("t_celaka")
+        DataGridView3.ReadOnly = True
+        DataGridView3.Columns(0).HeaderText = "KODE TRANSAKSI" '
+        DataGridView3.Columns(1).HeaderText = "STATUS"
+        DataGridView3.Columns(2).HeaderText = "TANGGAL"
+        DataGridView3.Columns(3).HeaderText = "NO KPA" '
+        DataGridView3.Columns(4).HeaderText = "NPK"
+        DataGridView3.Columns(5).HeaderText = "NAMA"
+        DataGridView3.Columns(6).HeaderText = "TEMPAT KK"
+        DataGridView3.Columns(7).HeaderText = "TEMPAT RAWAT"
+        DataGridView3.Columns(8).HeaderText = "TANGGAL KK"
+        DataGridView3.Columns(9).HeaderText = "KETERANGAN"
+    End Sub
+
+    'Method untuk menampilkan hasil kueri pencarian proses kecelakaan ke datagridview
+    Sub CariProsesKecelakaan()
+        kueri = TextBox4.Text.ToString.ToUpper
+        sql = "select b.id_tran,b.status,b.no_laka, a.tgl_cr,a.npk,c.nama,a.ket" &
+            " from t_celaka a left join t_celaka_pro b on b.no_laka=a.id_tran left join" &
+            " karyawan c on a.npk=c.npk where a.status='OPEN' and c.nama like '%" + kueri + "%' order by b.id_tran"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_celaka_pro")
+        DataGridView4.DataSource = ds.Tables("t_celaka_pro")
+        DataGridView4.ReadOnly = True
+        DataGridView4.Columns(0).HeaderText = "KODE TRANSAKSI" '
+        DataGridView4.Columns(1).HeaderText = "STATUS"
+        DataGridView4.Columns(2).HeaderText = "KODE TRAN PENGAJUAN"
+        DataGridView4.Columns(3).HeaderText = "NPK" '
+        DataGridView4.Columns(4).HeaderText = "NAMA"
+        DataGridView4.Columns(5).HeaderText = "TEMPAT KK"
+    End Sub
+
+    'Method untuk load kueri pencarian mutasi jabatan ke dataset untuk dicetak ke crystalreport
+    Sub loadHasil1()
+        kueri = TextBox1.Text.ToString.ToUpper
+        sql = "select a.id_tran,a.status,to_char(a.tgl_cr, 'fmdd MON yyyy')as tgl_cr,a.npk_1,b.nama," &
+                " d.nama_departemen, a.npk_2, e.nama as nama_proses,f.n_jabatan as jabatan_lama, g.n_jabatan as jabatan_baru," &
+                " a.ket,  h.nama as dibuat, l.n_jabatan as jab_buat, i.nama as diperiksa, m.n_jabatan as jab_periksa," &
+                " j.nama as disetujui, n.n_jabatan as jab_setuju, k.nama as mengetahui, o.n_jabatan as jab_mengetahui" &
+                " from t_mutasi_jab a join karyawan b on a.npk_1=b.npk join jab c on b.jab=c.id_jabatan" &
+                " join dept d on b.dept=d.id_departemen join karyawan e on e.npk=a.npk_2" &
+                " join jab f on a.n_jab=f.id_jabatan join jab g on a.o_jab=g.id_jabatan" &
+                " left join karyawan h on h.npk=a.lev_1 left join karyawan i on i.npk=a.lev_2" &
+                " left join karyawan j on j.npk=a.lev_3 left join karyawan k on k.npk=a.lev_4" &
+                " left join jab l on l.id_jabatan=h.jab left join jab m on m.id_jabatan=i.jab left" &
+                " join jab n on n.id_jabatan=j.jab left join jab o on o.id_jabatan=k.jab where b.nama like '%" + kueri + "%'"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_mutasi_jab")
+    End Sub
+
+    'Method untuk load kueri pencarian proses mutasi jabatan ke dataset untuk di cetak ke crystal report
+    Sub loadHasil2()
+        kueri = TextBox2.Text.ToString.ToUpper
+        sql = "select b.id_tran, b.status, to_char(b.tgl_cr, 'fmdd MON yyyy')as tgl_cr, a.id_tran as kode_permintaan, k.nama," &
+            " o.nama_departemen,l.n_jabatan as jab_lama, m.n_jabatan as jab_baru, n.n_jkary," &
+            " b.no_tap, to_char(b.tgl_tap, 'fmdd MON yyyy')as tgl_tap, b.ket," &
+            " c.nama as dibuat, g.n_jabatan as jab_buat, d.nama as diperiksa, h.n_jabatan as jab_periksa," &
+            " e.nama as disetujui, i.n_jabatan as jab_setuju, f.nama as mengetahui, j.n_jabatan as jab_mengetahui" &
+            " from t_mutasi_jab a left join t_mutasi_jab_pro b on b.id_minta_mutasi=a.id_tran" &
+            " left join karyawan c on c.npk=a.lev_1 left join karyawan d on d.npk=a.lev_2" &
+            " left join karyawan e on e.npk=a.lev_3 left join karyawan f on f.npk=a.lev_4" &
+            " left join jab g on g.id_jabatan=c.jab left join jab h on h.id_jabatan=d.jab" &
+            " left join jab i on i.id_jabatan=e.jab left join jab j on j.id_jabatan=f.jab" &
+            " left join karyawan k on k.npk=a.npk_2 left join jab l on l.id_jabatan=a.o_jab" &
+            " left join jab m on m.id_jabatan=a.n_jab left join j_kary n on n.id_jkary=k.j_kary" &
+            " left join dept o on o.id_departemen=k.dept" &
+            " where b.status='OPEN' and k.nama like '%" + kueri + "%'"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_mutasi_jab_pro")
+    End Sub
+
+    'Method untuk load hasil kueri pencarian kecelakaan kerja ke dataset untuk di cetak ke crystal report
+    Sub LoadKecelakaan1()
+        kueri = TextBox3.Text.ToString.ToUpper
+        sql = "select a.id_tran,a.status,to_char(a.tgl_cr, 'fmdd MON yyyy')as tgl_cr,a.no_kpa,a.npk,b.nama,a.tkp,a.t_rawat,to_char(a.tgl_tkp, 'fmdd MON yyyy')as tgl_tkp,a.ket," &
+            "h.nama as pembuat,l.n_jabatan as jab_buat, i.nama as diperiksa, m.n_jabatan as jab_periksa," &
+            " j.nama as disetujui,n.n_jabatan as jab_setuju, k.nama as mengetahui, o.n_jabatan as jab_mengetahui" &
+            " from t_celaka a left join karyawan b on b.npk=a.npk left join karyawan h on h.npk=a.lev_1" &
+            " left join karyawan i on i.npk=a.lev_2 left join karyawan j on j.npk=a.lev_3" &
+            " left join karyawan k on k.npk=a.lev_4 left join jab l on l.id_jabatan=h.jab" &
+            " left join jab m on m.id_jabatan=i.jab left join jab n on n.id_jabatan=j.jab" &
+            " left join jab o on o.id_jabatan=k.jab where b.nama like '%" + kueri + "%' order by a.id_tran"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_celaka")
+    End Sub
+
+    'Method untuk load hasil kueri pencarian proses kecelakaan kerja ke dataset untuk di cetak ke crystal report
+    Sub LoadKecelakaan2()
+        kueri = TextBox4.Text.ToString.ToUpper
+        sql = "select b.id_tran,b.status,b.no_laka, to_char(a.tgl_cr, 'fmdd MON yyyy')as tgl_cr,a.npk,c.nama,a.ket, d.nama as dibuat," &
+            " e.n_jabatan as jab_buat, f.nama as diperiksa, g.n_jabatan as jab_periksa, h.nama as disetujui, i.n_jabatan as jab_setuju, j.nama as mengetahui, k.n_jabatan as jab_mengetahui" &
+            " from t_celaka a left join t_celaka_pro b on b.no_laka=a.id_tran" &
+            " left join karyawan c on a.npk=c.npk left join karyawan d on d.npk=b.lev_1" &
+            " left join jab e on e.id_jabatan=d.jab left join karyawan f on f.npk=b.lev_2" &
+            " left join jab g on g.id_jabatan=f.jab left join karyawan h on h.npk=b.lev_3" &
+            " left join jab i on i.id_jabatan=h.jab left join karyawan j on j.npk=b.lev_4" &
+            " left join jab k on k.id_jabatan=j.jab and c.nama like '%" + kueri + "%' order by b.id_tran"
+        da = New OracleDataAdapter(sql, con)
+        ds = New DataSet
+        da.Fill(ds, "t_celaka_pro")
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        Call koneksi()
+        Call loadHasil1()
+        Form2.Show()
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        Call koneksi()
+        Call loadHasil2()
+        Form3.Show()
+    End Sub
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        Call koneksi()
+        Call LoadKecelakaan1()
+        Form4.Show()
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        Call koneksi()
+        Call LoadKecelakaan2()
+        Form5.Show()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox1.TextChanged
+        Call koneksi()
+        Call CariMutasi()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox2.TextChanged
+        Call koneksi()
+        Call CariProsesMutasi()
+    End Sub
+
+    Private Sub TextBox3_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox3.TextChanged
+        Call koneksi()
+        Call CariKecelakaanKerja()
+    End Sub
+
+    Private Sub TextBox4_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TextBox4.TextChanged
+        Call koneksi()
+        Call CariProsesKecelakaan()
+    End Sub
+End Class
